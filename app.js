fa18eab7060383e8445b7e4beedff0426f56e69f
Administrator <admin@example.com>
2021-04-04 15:53:02 +0900

    lineへの通知に変更

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
 const axios = require('axios')
-const postJSON = require('./post.json')
+const qs = require('querystring')
 require('dotenv').config()
 
-const teamsURL = process.env.TEAMS_WEBHOOK_URL
+const LINE_TOKEN = process.env.LINE_TOKEN
+const LINE_URL = 'https://notify-api.line.me/api/notify'
+const HP = 'https://unkou.keikyu.co.jp/'
+const TWITTER = 'https://twitter.com/keikyu_official'
 
-const postTeams = async () => {
-    const res = await axios
-                        .post(teamsURL, postJSON)
-                        .catch((error) => console.log(error))
-    console.log(`statusCode: ${res.data}`)
+const message = `\n京急線の遅延があります\n
+                ${HP}\n
+                ${TWITTER}`
+
+const config = {
+    url: LINE_URL,
+    method: 'post',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': `Bearer ${LINE_TOKEN}`
+    },
+    data: qs.stringify({
+        message: message
+    })
+}
+
+const postLine = async () => {
+    axios
+    .request(config)
+    .then((res) => console.log(res.data))
+    .catch((error) => console.log(error))
 }
 
 const getTrainDelayInfo = async () => {
     await axios
             .get('https://tetsudo.rti-giken.jp/free/delay.json')
             .then((res) => res.data.some((info) => info.name === '京急線'))
-            .then((isDelay) => isDelay && postTeams())
+            .then((isDelay) => isDelay && postLine())
             .catch((error) => console.log(error))
 }
 
